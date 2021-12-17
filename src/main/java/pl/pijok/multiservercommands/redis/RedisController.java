5aec6c108524c159e2b42e1b5411e2c6cd471e51
Pijook <lordbaby330@gmail.com>
2021-12-17 11:03:38 +0100

    Changed java compile version to 1.8

--- src/main/java/pl/pijok/multiservercommands/redis/RedisController.java
+++ src/main/java/pl/pijok/multiservercommands/redis/RedisController.java
@@ -46,7 +46,7 @@ public class RedisController {
                             });
                         }
                     }
-                });
+                }, channel);
             }
         }).start();
     }
